e1c4d0444167a344851bc716db61e29d113442fa
sorceriv <sorceriv@gmail.com>
2024-04-14 15:04:47 +0800

    Flutter testing

---
+++ flutter_loconnected/flutter_loconnected/android/app/src/main/kotlin/com/LoConnected/flutter_loconnected/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.LoConnected.flutter_loconnected
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_loconnected/flutter_loconnected/ios/Runner/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBOaosExkcjs_RkXNSyOqeXyR5xzI6lKo4")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
