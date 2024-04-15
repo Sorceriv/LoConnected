f52621d50700bb4cd142c87895e0bfe7629ab693
sorceriv <sorceriv@gmail.com>
2024-04-15 12:34:15 +0800

    Experimenting with hiding API keys

---
+++ flutter_loconnected/flutter_loconnected/lib/pages/google_map_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapPage extends StatefulWidget {
+  const GoogleMapPage({super.key});
+
+  @override
+  State<GoogleMapPage> createState() => _GoogleMapPageState();
+}
+
+class _GoogleMapPageState extends State<GoogleMapPage> {
+  static const googlePlex = LatLng(37.4223, -122.0848);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: googlePlex,
+            zoom: 13,
+          ),
+        ),
+      );
+}
